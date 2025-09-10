bdfd253118c0434a5796d7548a01db62beece206
risko67 <ciprich.richard@gmail.com>
2025-09-10 10:09:13 +0200

    meno vek gender 005...

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 15;
+            Console.WriteLine(myAge);
+
+            float pi = 6.7f;
+            Console.WriteLine(pi);
+
+            string name = "Risko";
+            Console.WriteLine(name);
+
+            char gender = 'M';
+
+            Console.WriteLine(gender);
+
+            bool isChild = true;
+            Console.WriteLine(isChild);
+        }
+    }
+}
