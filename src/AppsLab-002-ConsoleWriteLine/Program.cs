1fb7f76be39ed1cee13d2361acaf2e7809e10d95
risko67 <ciprich.richard@gmail.com>
2025-09-10 08:55:20 +0200

    zmenil som 4 riadky a jeden vynechal

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//napise text 
+Console.WriteLine("Ahoj, Riso! 1 ");
+Console.WriteLine("Ahoj, Riso! 2 ");
+//Console.WriteLine("Ahoj, Riso! 3 ");
+Console.WriteLine("Ahoj, Riso! 4 ");
